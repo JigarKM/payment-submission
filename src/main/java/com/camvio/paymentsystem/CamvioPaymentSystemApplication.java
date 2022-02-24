24da1fb4d7f03389a021d15f628817da549d575b
jigar <jmadhani47@gmail.com>
2022-02-23 21:26:34 -0600

    [CAMVIO-1] project setup

---
+++ src/main/java/com/camvio/paymentsystem/CamvioPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.camvio.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamvioPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamvioPaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camvio/paymentsystem/CamvioPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camvio.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamvioPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
